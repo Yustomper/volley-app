e2390ce0cde85da0c14c16c3f9a422aa1fda3f01
Guido Perez <gperzal@gmail.com>
2024-10-03 00:15:35 -0300

    :hamme: test x url

--- volleyball-back/volley_back/urls.py
+++ volleyball-back/volley_back/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('', include('docs.urls')),
-    path('api/', include('teams.urls')),
+    path('api/teams/', include('teams.urls')),
     path('api/matches/', include('matches.urls')),
-    path('api/', include('statistic.urls'))
-
+    path('api/statistics/', include('statistic.urls')),
 ]
